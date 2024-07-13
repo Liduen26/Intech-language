0716adca744df4862f7194cde9fa6aabd500b49e
Snowky92 <amaurypinheiro@hotmail.fr>
2024-07-13 11:54:19 +0200

    Update sym_table.h

--- sym_table.h
+++ sym_table.h
@@ -9,9 +9,9 @@ typedef struct sym_table_t {
 } sym_table_t;
 
 ast_list_t *sym_list_new_node (ast_t *elem);
-ast_list_t *sym_list_add (ast_list_t **prev_node_list, ast_t *elem);
+ast_list_t *sym_list_add (sym_table_t **prev_node_list, ast_t *elem);
 
-void printList(ast_list_t *node_list);
+void printList(sym_table_t *node_list);
 
 
 #endif // Sym_TABLE_H
