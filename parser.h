0e250333e0c9045fc3c2719753c4696498b38b33
Liduen <75698473+Liduen26@users.noreply.github.com>
2024-07-14 14:00:55 +0200

    Suite analyse instructions et .h

--- parser.h
+++ parser.h
@@ -18,8 +18,11 @@ ast_t* analyse_function(sym_table_t *global_sym_table, buffer_t *buffer);
 ast_list_t* analyse_param(buffer_t *buffer, ast_list_t *list_param, sym_table_t *local_table);
 var_type_e analyse_return(buffer_t *buffer);
 ast_list_t* analyse_corps(buffer_t *buffer, ast_list_t *list_instructions, sym_table_t *global_sym_table, sym_table_t *local_table);
+ast_list_t* analyse_args(buffer_t *buffer, ast_list_t *list_args, sym_table_t *global_sym_table, sym_table_t *local_table);
 
 void check_valid_name(buffer_t *buffer);
+bool is_function(buffer_t *buffer);
+bool is_conditional_operator(const char *op);
 var_type_e type_str_to_enum(char* type_str);
 
 #endif // PARSER_H
