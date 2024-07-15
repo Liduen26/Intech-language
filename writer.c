2aff53498030994e39cb0ca5a7042ea629aab03b
Liduen <75698473+Liduen26@users.noreply.github.com>
2024-07-15 23:38:55 +0200

    PC writer

---
+++ writer.c
@@ -0,0 +1,12 @@
+/*
+Y doit
+récup l'ast head que renvoie le parser (function main)
+gros switch case sur le type de l'ast (ast_node_type_e)
+Case function : 
+    printf("function %s (%... ) : %s", fucntion.name, funciton.param, function.return_type)
+case declr_var: 
+    printf("let %s : %s", ...)
+
+
+
+*/
